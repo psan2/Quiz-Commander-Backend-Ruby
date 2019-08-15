811a0ef027d7fd7d62b5ac550fceda98ddf46867
Pat <patrick.s.santucci@gmail.com>
2019-08-15 17:50:11 +0100

    bugfixes for quizzes controller and added questions and answers to quiz serializer

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,4 +6,5 @@ class Quiz < ApplicationRecord
   has_many :quiz_rounds
   has_many :rounds, through: :quiz_rounds
   has_many :questions, through: :rounds
+  has_many :answers, through: :questions
 end
